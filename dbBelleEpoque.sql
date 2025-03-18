2bf18432a716ce7003ba75702680f7d77364d5ad
juliaporf <157625969+juliaporf@users.noreply.github.com>
2025-03-18 08:08:21 -0300

    modificando o banco

--- dbBelleEpoque.sql
+++ dbBelleEpoque.sql
@@ -1,24 +1,2 @@
-select * from setor
-
-Alter table setor
-drop column roupa,
-drop column calçados,
-drop column bolsas,
-drop column acessorio;
-
-Alter table setor
-add column tipo varchar(300)
-
-Alter table setor 
-alter column tipo type char(1)
-
-Alter table produto
-add column imagem varchar(300)
-
-select * from usuario
-
-Alter table usuario 
-alter column status_cliente type char(1)
-
-/*Setor- tipo- roupas=1, calçados=2, bolsas=3 e acessorios=4 */
-/*Produto- status_cliente- cliente=1, admim=2*/
+alter table produto
+drop column img

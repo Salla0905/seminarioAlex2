f41304d42adf90c0a9de9ff32b04171be7a0ba9a
Salla0905 <bruna.salla@germinare.org.br>
2025-04-12 17:30:52 -0300

    corrigindo erro 405

--- BelleEpoque/src/main/java/org/example/belleepoque/controller/LoginController.java
+++ BelleEpoque/src/main/java/org/example/belleepoque/controller/LoginController.java
@@ -1,10 +1,10 @@
 package org.example.belleepoque.Controller;
 
 import org.example.belleepoque.dto.UsuarioDTO;
-import org.example.belleepoque.repository.UsuarioRepository;
 import org.example.belleepoque.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,14 +13,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/login")
 public class LoginController {
 
-    @Autowired // ← INJEÇÃO DO SERVICE
+    @Autowired
     private UsuarioService usuarioService;
-    @PostMapping("/login")
+
+    @GetMapping
+    public String mostrarLogin() {
+        return "login";
+    }
+
+    @PostMapping
     public String login(@ModelAttribute UsuarioDTO usuarioDTO) {
         if (usuarioService.validarLogin(usuarioDTO.getEmail(), usuarioDTO.getSenha())) {
             return "redirect:/home";
         } else {
-            return "redirect:/login?erro=true"; // Redireciona com flag de erro
+            return "redirect:/login?erro=true";
         }
     }
 }

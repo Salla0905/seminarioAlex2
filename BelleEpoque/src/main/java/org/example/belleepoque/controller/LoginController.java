f3dd04724bf05959261c08740a8c3577042a64a7
Salla0905 <bruna.salla@germinare.org.br>
2025-04-13 12:11:35 -0300

    corrigindo erro 500

--- BelleEpoque/src/main/java/org/example/belleepoque/controller/LoginController.java
+++ BelleEpoque/src/main/java/org/example/belleepoque/controller/LoginController.java
@@ -4,12 +4,9 @@ import org.example.belleepoque.dto.UsuarioDTO;
 import org.example.belleepoque.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/login")
 public class LoginController {
 
